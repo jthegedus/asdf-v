91ba3f1178dbf7d54cd58e464c77a51068e5ffa4
jthegedus <jthegedus@hey.com>
2022-07-06 23:34:42 +1000

    chore: simplify again

--- lib/utils.bash
+++ lib/utils.bash
@@ -10,10 +10,3 @@ function fail() {
 	printf "asdf-%s: %s\\n" "$TOOL_NAME" "$*"
 	exit 1
 }
-
-curl_opts=(-fsSL)
-
-# NOTE: You might want to remove this if plugin-template-test is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
