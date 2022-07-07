b1350d33d4c2cba11a7750084d683956443928d2
jthegedus <jthegedus@hey.com>
2022-07-08 02:35:45 +1000

    fix: omit bad git tags. validate versions on install

--- lib/envs.bash
+++ lib/envs.bash
@@ -5,3 +5,6 @@ set -euo pipefail
 GH_REPO="https://github.com/vlang/v"
 TOOL_NAME="v"
 TOOL_TEST="v --help"
+MIN_SUPPORTED_VERSION="0.3"
+MIN_SUPPORTED_WEEKLY_VERSION="weekly.2022.26"
+VERSIONS_TO_OMIT="mytest" # pipe-separated list. Eg: "mytest|0.2". grep is used, so partial matches.
